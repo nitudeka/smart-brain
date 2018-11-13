b9871a51ae294c6b01460b1ea432889cf47f7572
nitudeka <nitumonideka327@gmail.com>
2018-11-13 14:33:15 +0530

    Updates

---
+++ precache-manifest.f215116da1fba89ed459cedfc365c789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe10683db579973390e18070d5423a8d",
+    "url": "/smart_brain/static/media/bg-image.fe10683d.jpg"
+  },
+  {
+    "revision": "e79918ea82bcf916e244",
+    "url": "/smart_brain/static/js/runtime~main.e79918ea.js"
+  },
+  {
+    "revision": "3b248d7468786f40c044",
+    "url": "/smart_brain/static/js/main.3b248d74.chunk.js"
+  },
+  {
+    "revision": "7eb718b758b4968eaaaa",
+    "url": "/smart_brain/static/js/1.7eb718b7.chunk.js"
+  },
+  {
+    "revision": "3b248d7468786f40c044",
+    "url": "/smart_brain/static/css/main.a04cc115.chunk.css"
+  },
+  {
+    "revision": "7eb718b758b4968eaaaa",
+    "url": "/smart_brain/static/css/1.9f120a32.chunk.css"
+  },
+  {
+    "revision": "f93808d4a10f4a1dd3f5242e5842d139",
+    "url": "/smart_brain/index.html"
+  }
+];
